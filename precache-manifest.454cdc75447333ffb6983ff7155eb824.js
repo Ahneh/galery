de03e03f9bf0638fe4e061c7766e72cddea488f0
Artur <ahneh7@gmail.com>
2021-02-03 20:03:21 +0200

    Updates

---
+++ precache-manifest.454cdc75447333ffb6983ff7155eb824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd30895d78882560b18c8cdc902721b",
+    "url": "/galery/index.html"
+  },
+  {
+    "revision": "1a07eb8accd405520a6d",
+    "url": "/galery/static/css/main.92a82b78.chunk.css"
+  },
+  {
+    "revision": "2f4ef52a6f5682a31153",
+    "url": "/galery/static/js/2.cbd4021c.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/galery/static/js/2.cbd4021c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a07eb8accd405520a6d",
+    "url": "/galery/static/js/main.f952e3c6.chunk.js"
+  },
+  {
+    "revision": "f3e7df2d65b314c8aa5c",
+    "url": "/galery/static/js/runtime-main.a4f6e18c.js"
+  }
+]);
